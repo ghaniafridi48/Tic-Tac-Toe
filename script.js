1668693028fb3ddd2845bf8601f957e989a767b2
ghaniafridi48 <pshfscmm2000548@gmail.com>
2026-09-05 13:00:59 +0500

    add win condition check to display win

--- script.js
+++ script.js
@@ -21,13 +21,31 @@ let createPlayer = (name,marker) => {
 };
 
 let flow = {
-      
+
+    winCombos : [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],
+
+    checkwin(){
+        for (let i = 0; i < this.winCombos.length; i++) {
+                let combo = this.winCombos[i];
+                let firstPos = Gameboard.gameboard[combo[0]]
+                let secondPos = Gameboard.gameboard[combo[1]]
+                let thirdPos = Gameboard.gameboard[combo[2]] 
+                
+                if(firstPos !== undefined && firstPos === secondPos && secondPos === thirdPos){
+                    return true;
+                }
+            }
+        return fasle
+    }  
 }
 
 const player1 =  createPlayer('me','x');
+player1.fillarr(0);
+player1.fillarr(1);
 player1.fillarr(2);
 
 const player2 = createPlayer('opponenet','o');
-player2.fillarr(1);
+player2.fillarr(5);
 
 console.log(Gameboard.gameboard);
+console.log(flow.checkwin());
