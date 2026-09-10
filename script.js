63ccd6bc57723d17741b15107d7ba86ffb3d41f7
ghaniafridi48 <pshfscmm2000548@gmail.com>
2026-09-10 17:38:54 +0500

    refactor player creation and game logic; enhance win message display

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
 let player1;
+let player2;
+let currentplayer;
 
 const playerBtn = document.getElementById('playerBtn');
         playerBtn.addEventListener('click' , () => {
-           let userName = prompt('Name : ');
-           let userMarker = prompt('marker : ');
-           player1 = createPlayer(userName,userMarker);
+            let userName1 = prompt('Player 1 Name: ');
+            let userMarker1 = prompt('Player 1 Marker (X or O): ');
+            player1 = createPlayer(userName1, userMarker1);
+
+            let userName2 = prompt('Player 2 Name: ');
+            let userMarker2 = prompt('Player 2 Marker (X or O): ');
+            player2 = createPlayer(userName2, userMarker2);
+
+            currentplayer = player1;
+
         })
         
 let Gameboard = (function(){
-    let board = ["","","","","","","","",""];
+        let board = ["","","","","","","","",""];
 
-    return {
-        gameboard : board
-    };
+        const getBoard = () => board;
+
+        const resetBoard = () => {
+            for (let i = 0; i < board.length; i++) {
+                    board[i] = "";            
+            }
+        };
+
+        const setMarker = (index,marker) =>{
+            board[index] = marker;
+
+        };
+
+        
+        return {
+            getBoard,
+            resetBoard,
+            setMarker
+        };
 })();
 
 let createPlayer = (name,marker) => {
-
     return{
-
-        name : name,
-        marker : marker,
-
-        fillarr (index) {
-            if (Gameboard.gameboard[index] !== "" ) {
-                console.log('this position already has a marker');
-            }else{
-                Gameboard.gameboard[index] = this.marker;
-            }
-
-        }
-    };
-
+        name,
+        marker
+    }
 };
 
 let flow = {
@@ -41,15 +54,15 @@ let flow = {
     checkwin(){
         for (let i = 0; i < this.winCombos.length; i++) {
                 let combo = this.winCombos[i];
-                let firstPos = Gameboard.gameboard[combo[0]]
-                let secondPos = Gameboard.gameboard[combo[1]]
-                let thirdPos = Gameboard.gameboard[combo[2]] 
+                let firstPos = Gameboard.getBoard()[combo[0]]
+                let secondPos = Gameboard.getBoard()[combo[1]]
+                let thirdPos = Gameboard.getBoard()[combo[2]] 
                 
                 if(firstPos !== "" && firstPos === secondPos && secondPos === thirdPos){
-                    return true;
+                    document.getElementById('message').textContent = `${currentplayer.name} wins!`;
+
                 }
             }
-        return false;
     }  
 }
 
@@ -59,15 +72,18 @@ const display = {
         const cell = document.querySelectorAll('.cell');
         cell.forEach((singleCell) => {
             singleCell.addEventListener("click", () =>{
+                 if (Gameboard.getBoard()[singleCell.dataset.index] === "" ) {
+                     
+                     Gameboard.setMarker(singleCell.dataset.index , currentplayer.marker);
+                     singleCell.textContent = Gameboard.getBoard()[singleCell.dataset.index];
 
-            player1.fillarr(singleCell.dataset.index);
-            flow.checkwin();
-            singleCell.textContent = Gameboard.gameboard[singleCell.dataset.index];
-
-        })
-    }) 
+                     flow.checkwin();
+                     
+                     (currentplayer === player1) ? currentplayer = player2 : currentplayer = player1;
+                     
+                    }
+                    })
+                }) 
+            }
 }
-}
-
 display.display();
-
